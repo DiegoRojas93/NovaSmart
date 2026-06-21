fbc4c355733d05b7d12d40ba2f78102a82d6acea
Diego Rojas <diegorojas431@gmail.com>
2026-06-20 20:14:49 -0500

    Escuchando los cambios de react

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    host: true, // Necesario para que Docker exponga el puerto
+    port: 5173, 
+    watch: {
+      usePolling: true, // 👈 Esta es la clave para que escuche cambios en Docker
+    }
+  }
 })
