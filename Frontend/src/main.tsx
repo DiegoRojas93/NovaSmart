1d7ef633b922e88abd73865bac2bbd5ad53bfaac
Diego Rojas <diegorojas431@gmail.com>
2026-07-06 16:57:04 -0500

    Añadiendo taildwind al proyecto

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-import Form2 from './Form2.tsx'
+import './styles/index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Form2 />
+    <App />
   </StrictMode>,
 )
